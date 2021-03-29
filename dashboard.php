55b070b9c622bcc922e52dcf0908f9d206bc3abc
Natasha-Simi <41041219+Natasha-Simi@users.noreply.github.com>
2021-03-29 11:19:08 +0300

    Revert "CHANGES"

--- dashboard.php
+++ dashboard.php
@@ -124,7 +124,6 @@
                                         
                         $sql= "SELECT DISTINCT floorNumber FROM floor";
                         $result = mysqli_query($conn, $sql);
-                        echo"<option value=".">"."CHOOSE FLOOR"."</option>";
                         while($row=mysqli_fetch_array($result)){
                             echo"<option value=".$row['floorNumber'].">".$row['floorNumber']."</option>";
                         }
@@ -273,15 +272,12 @@
                     success:function(response){
                         $('#s2 option').remove();
                         console.table(response);
-
-                        var holder ="<option value=''>"+"SELECT ROOM"+
-                                            
-                                        "</option>";
-                            $('#s2').append(holder);
-
                         for (var i = 0; i < response.length; i++) 
                         {
-                            var option =
+                            var option ="<option value=''>"+"SELECT ROOM"+
+                                            
+                                        "</option>"+
+
                                         "<option value='"+response[i].floorNumber+"'>"
                                             +response[i].roomName+
                                         "</option>";
